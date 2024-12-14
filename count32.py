6abcb089ddadf76d675003549e39c674bba87382
Mike Smith <mike_smith@harvard.edu>
2024-12-14 14:56:18 -0500

    Update count32.py to match update to ALEs 2.1-2.5

--- count32.py
+++ count32.py
@@ -3,9 +3,9 @@
 # Grab the text blurb to process
 import sys
 #print("sys.argv =", sys.argv)
-if (len(sys.argv) == 1):
+if len(sys.argv) == 1:
     blurb = input('Text: ')
-elif (len(sys.argv) == 2):
+elif len(sys.argv) == 2:
     input_file = sys.argv[1]
     with open('txts/' + input_file) as my_open_file:
         blurb = my_open_file.read()
@@ -15,18 +15,18 @@ else:
 # Count the number of non-blank lines in the blurb
 # using the Python string method `split`.
 lines = 0
-# REPLACE ME with a for-loop
+# ALE 2.2: REPLACE ME with a for-loop
 print(lines, "line(s)")
 
 # Count the number of alphabet characters in the blurb
 # using the Python string method `isalpha`.
 letters = 0
-# REPLACE ME with a for-loop
+# ALE 2.3: REPLACE ME with a for-loop
 print(letters, "letter(s)")
 
 # Count the number of words in the blurb, where a word
 # is any sequence of non-whitespace characters surrounded
 # by any amount of whitespace. Please use the Python string
 # method `split` and one function on sequences.
-words = 0 # REPLACE THE 0 with an expression
+words = 0 # ALE 2.4: REPLACE THE 0 with an expression
 print(words, "word(s)")
